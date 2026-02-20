5ebbbc6c5dd1c72f91c887dea1ace7a27c745c15
hong42 <hong42@vntgcorp.com>
2026-02-20 09:26:59 +0900

    APP

---
+++ client/android/app/src/main/java/com/hong/currencycalc/MainActivity.java
@@ -0,0 +1,5 @@
+package com.hong.currencycalc;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hong.currencycalc',
+  appName: 'Currency Calc',
+  webDir: 'dist'
+};
+
+export default config;
